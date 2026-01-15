2bfcf3c37e6d0a56910d1c04741dca7fde802c3a
Anup Kodlekere <100858233+anup-kodlekere@users.noreply.github.com>
2026-01-15 15:49:13 +0530

    Update validate_builds.py

--- gha-script/validate_builds.py
+++ gha-script/validate_builds.py
@@ -154,10 +154,10 @@ def trigger_script_validation_checks(file_name):
         "/home/tester/{}".format(file_name),
         network='host',
         detach=True,
-        volumes={
-            current_dir: {'/home/tester': {'mode': 'rw'}}
-        },
-        # volumes={"tester_data": {"bind": "/home/tester", "mode": "rw"}},
+        # volumes={
+        #     current_dir: {'/home/tester': {'mode': 'rw'}}
+        # },
+        volumes={"/home/tester": {"mode": "rw"}},
         stderr=True,  # Return logs from STDERR
 
     )
